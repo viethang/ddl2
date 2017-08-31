093d61da1aba03ac764e41638a2c76b365292d52
Viet Hang Nguyen <viethangybvn@gmail.com>
2017-08-31 22:48:21 +0200

    Add method to and register models

--- api/analytics/models.py
+++ api/analytics/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+
 class Student(models.Model):
     firstname = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     sciper = models.CharField(max_length=4)
 
+    def __str__(self):
+        return self.firstname + ' ' + self.lastname
+
 class Result(models.Model):
     gpa = models.FloatField()
     student = models.OneToOneField(Student)
+
+    def __str__(self):
+        return self.student.firstname + ' ' + self.student.lastname + ' -- ' + self.gpa
